8362ef6354b9a96b9081b4804db834a975745311
xujian <2513573453@qq.com>
2021-08-12 22:49:52 +0800

    feat:数据库连接配置

---
+++ db.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize')
+const CONFIG = require('./config')
+const sequelize = new Sequelize(CONFIG.MYSQL.database, CONFIG.MYSQL.username, CONFIG.MYSQL.password, {
+  host: CONFIG.MYSQL.host,
+  dialect: 'mysql',
+  logging: CONFIG.DEBUG ? console.log : false,
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+  timezone: '+08:00'
+})
+module.exports = sequelize
